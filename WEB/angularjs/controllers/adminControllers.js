e79c925975dc12053d91f87c1e21bba8bcf4fdcf
Adrian P <adrian111111a@gmail.com>
2017-03-21 13:53:46 +0100

    login view, authentication,, //TO DO add session cookie check

---
+++ WEB/angularjs/controllers/adminControllers.js
@@ -0,0 +1,40 @@
+﻿angular.module("usterkaAdmin")
+    .controller("authCtrl", ['$rootScope','$http','$location','AuthUrl', function ($scope, $http, $location, AuthUrl) {
+
+        $scope.authenticate = function (user, pass) {
+            AuthUrl.checkCredentials(user, pass).then( function ( result ) {
+                if(result.data && result.data.indexOf("error") == -1){
+                    $location.path("/main");
+                } else{
+                    $scope.authenticationError = result.data;
+                }
+            }, function(error){
+                $scope.authenticationError = error.data;
+            });
+
+        }
+    }])
+    .controller("mainCtrl", function ($scope) {
+        $scope.screens = ["Zgłoszenia", "Informacje", "Użytkownicy"];
+        $scope.current = $scope.screens[0];
+
+        $scope.setScreen = function (index) {
+            $scope.current = $scope.screens[index];
+        };
+
+        $scope.getScreen = function () {
+            var ret;
+            if ($scope.current == "Zgłoszenia") {
+                ret = "/views/adminReports.html";
+            }
+            else if ($scope.current == "Informacje") {
+                ret = "/views/adminInfo.html";
+            }
+            else {
+                ret = "/views/adminUsers.html";
+            }
+            return ret;
+
+        };
+    });;;
+
---
+++ WEB/angularjs/services/adminService.js
@@ -0,0 +1,29 @@
+/**
+ * Created by apanczyk on 2017-03-15.
+ */
+angular.module("usterkaAdmin")
+.factory("AuthUrl", ['$http', '$cookies', '$q',  function ($http, $cookies, $q) {
+
+
+        return {
+            checkCredentials: function (user, pass) {
+                var deferred = $q.defer();
+                $http({
+                    method: "POST",
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    url: 'http://tranquil-hamlet-1905.herokuapp.com/users/login',
+                    data: $.param({username: user, password: pass})
+                }).then(function (response) {
+                    console.log(response);
+                    $cookies.put('authCookie', response);
+                    deferred.resolve( response );
+                }).catch(function (response) {
+                    deferred.resolve( response );
+                });
+
+                return deferred.promise;
+            }
+        };
+
+
+}]);
